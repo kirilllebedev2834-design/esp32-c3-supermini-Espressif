07336b97c42e6c7fb386fa55cc979220793143db
Kirill76 <kirilllebedev2834@gmail.com>
2026-07-22 17:34:38 +0300

    изменен

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -26,30 +26,81 @@
 */
 #include "macrosandother.h"
 
+#define REGISTER_POINTER(adress) (*(volatile uint32_t*)(adress))
+
+/*
+Инициализация UARTn
+Для инициализации UARTn:
+
+• включите тактирование RAM UART, установив SYSTEM_UART_MEM_CLK_EN в 1;
+• включите APB_CLK для UARTn, установив SYSTEM_UARTn_CLK_EN в 1;
+• сбросьте SYSTEM_UARTn_RST;
+• запишите 1 в UART_RST_CORE;
+• запишите 1 в SYSTEM_UARTn_RST;
+• сбросьте SYSTEM_UARTn_RST;
+• сбросьте UART_RST_CORE;
+• включите синхронизацию регистров, сбросив UART_UPDATE_CTRL.
+
+Настройка связи UARTn
+
+Для настройки связи UARTn:
+• дождитесь, пока UART_REG_UPDATE станет равным 0, что указывает на завершение последней синхронизации;
+• настройте статические регистры (если таковые имеются) в соответствии с разделом 26.5.1.2;
+• выберите источник тактового сигнала через UART_SCLK_SEL;
+• настройте коэффициент деления делителя через UART_SCLK_DIV_NUM, UART_SCLK_DIV_A и UART_SCLK_DIV_B;
+• настройте скорость передачи (боудрейт) через UART_CLKDIV и UART_CLKDIV_FRAG;
+• настройте длину данных через UART_BIT_NUM;
+• настройте контроль чётности (нечётный или чётный) через UART_PARITY_EN и UART_PARITY;
+• дополнительные шаги в зависимости от приложения ...
+• синхронизируйте настроенные значения с доменом Core Clock, записав 1 в UART_REG_UPDATE.
+
+*/
 void init_uart1(void)
 {
-    /*
-        To initialize UARTn:
-        • enable the clock for UART RAM by setting SYSTEM_UART_MEM_CLK_EN to 1;
-        • enable APB_CLK for UARTn by setting SYSTEM_UARTn_CLK_EN to 1;
-        • clear SYSTEM_UARTn_RST;
-        • write 1 to UART_RST_CORE;
-        • write 1 to SYSTEM_UARTn_RST;
-        • clear SYSTEM_UARTn_RST;
-        • clear UART_RST_CORE;
-        • enable register synchronization by clearing UART_UPDATE_CTRL
-    */
-    
+    REGISTER_POINTER(SYS_CLK_EN0_REG) |= (1 << 24) | (1 << 5);
+
+    REGISTER_POINTER(SYS_RST_EN0_REG) &= ~(1 << 5);
+
+    REGISTER_POINTER(UART1_CLK_CONF_REG) |= (1 << 23);
+
+    REGISTER_POINTER(SYS_RST_EN0_REG) |= (1 << 5);
+
+    REGISTER_POINTER(SYS_RST_EN0_REG) &= ~(1 << 5);
+
+    REGISTER_POINTER(UART1_CLK_CONF_REG) &= ~(1 << 23);
+
+    REGISTER_POINTER(UART1_UPDATE_REG) &= ~(1 << 0);
+
+
+    // • дождитесь, пока UART_REG_UPDATE станет равным 0 (завершение синхронизации)
+    while ((REGISTER_POINTER(UART1_UPDATE_REG) & (1 << 0)) != 0) {}
+
+    // • выберите источник тактового сигнала APB_CLK 80 МГц (биты 21:20 = 01),
+    //   а также включите тактирование TX/RX (биты 25, 24, 22) и базовый делитель (биты 19:12 = 1)
+    REGISTER_POINTER(UART1_CLK_CONF_REG) = (1 << 25) | (1 << 24) | (1 << 22) | (1 << 20) | (1 << 12);
+
+    // • настройте скорость передачи (115200 бод при частоте APB 80 МГц ( 80млн/115200 ~~ 694,(4) ): по формуле:
+    //итоговая скорость передачи равна
+    //INPUT_FREQ / (UART_CLKDIV + UART_CLKDIV_FRAG / 16)
+    //где INPUT_FREQ — частота источника тактового сигнала ядра UART. целая часть 694, дробная 7)
+    //   записываем в UART_CLKDIV (биты [23:4]) и UART_CLKDIV_FRAG (биты [3:0])
+    REGISTER_POINTER(UART1_CLKDIV_REG) = (694 << 4) | 7;
+
+    // • настройте длину данных 8 бит (значение 11b в битах [3:2]) и отключите четность
+    REGISTER_POINTER(UART1_CONF0_REG) = (3 << 2);
+
+    // • синхронизируйте настроенные значения с доменом Core Clock, записав 1 в UART_REG_UPDATE (бит 0)
+    REGISTER_POINTER(UART1_UPDATE_REG) |= (1 << 0);
 }
 
 void init_gpio(void)
 {
     // GPIO8 - светодиод
-    *(volatile uint32_t*)(IO_MUX_CONF_REG_FOR_PIN_GPIO8) = (1 << 12);
-    *(volatile uint32_t*)(GPIO_OUTPUT_ENABLE_REGISTER) |= (1 << 8);
+    REGISTER_POINTER(IO_MUX_CONF_REG_FOR_PIN_GPIO8) = (1 << 12);
+    REGISTER_POINTER(GPIO_OUTPUT_ENABLE_REGISTER) |= (1 << 8);
     // GPIO9 - кнопка boot 
-    *(volatile uint32_t*)(IO_MUX_CONF_REG_FOR_PIN_GPIO9) = (1 << 12);
-    *(volatile uint32_t*)(IO_MUX_CONF_REG_FOR_PIN_GPIO9) |= (1 << 9);
+    REGISTER_POINTER(IO_MUX_CONF_REG_FOR_PIN_GPIO9) = (1 << 12);
+    REGISTER_POINTER(IO_MUX_CONF_REG_FOR_PIN_GPIO9) |= (1 << 9);
 }
 
 void app_main(void) 
