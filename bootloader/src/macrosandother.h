ad973d710e5f39aebf871a285897465a2b57f52d
Kirill76 <kirilllebedev2834@gmail.com>
2026-07-23 15:35:40 +0300

    написаны прототипы функций led_on, uart_print sys_print_error и перечесления ошибок

--- bootloader/src/macrosandother.h
+++ bootloader/src/macrosandother.h
@@ -16,15 +16,13 @@
 #define bool _Bool
 
 // UART macroses for work from documentation:
-#define UART1_BASE_ADRESS 0x60010000
-#define UART1_CLK_DEV_REG 0x60010014
+#define UART_FIFO_REG 0x60000000  // Буфер приема/передачи (FIFO)
+#define UART_CLKDIV_REG 0x60000014  // Делитель частоты (скорость)
+#define UART_CONF0_REG  0x60000020  // Конфигурация кадра
+#define UART_CLK_CONF_REG 0x60000078  // Настройка частоты ядра
+#define UART_UPDATE_REG 0x600000A4  // Синхронизация регистров
 #define SYS_CLK_EN0_REG 0x600C0010  // Включение тактирования периферии
 #define SYS_RST_EN0_REG 0x600C0018  // Сброс периферии
-#define UART1_CLKDIV_REG 0x60010014  // Делитель частоты (скорость)
-#define UART1_CONF0_REG  0x60010020  // Конфигурация кадра
-#define UART1_CLK_CONF_REG 0x60010078  // Настройка частоты ядра
-#define UART1_UPDATE_REG 0x600100A4  // Синхронизация регистров
-#define UART1_FIFO_REG 0x60010000  // Буфер приема/передачи (FIFO)
 
 //GPIO and IO_MUX macroses for work from documentation: 
 #define GPIO_OUTPUT_ENABLE_REGISTER 0x60004020
@@ -35,13 +33,26 @@
 #define IO_MUX_CONF_REG_FOR_PIN_GPIO5 0x6000901C
 #define IO_MUX_CONF_REG_FOR_PIN_GPIO4 0x60009018
 
-
 typedef uint8_t byte;
+
+// перечесления ошибок 
+typedef enum
+{
+   SYSTEM_IS_OK = 0,
+   SYSTEM_TIMEOUT = 1,
+   SYSTEM_INVALID_ARG = 2,
+   SYSTEM_PER_IS_NOT_READY = 3,
+   SYSTEM_INVALID_JUMP_TO_PROGRAM = 4
+} error_status_t; 
+
 
 extern void delay(uint16_t milliseconds); 
 extern void blink_function(uint16_t count, uint32_t delay_milliseconds);
 //extern void 
 void init_uart1(void);
 void init_gpio(void);
+void led_on(void);
+void uart_print(const char* string);
+void sys_print_error(const char* message);
 
 #endif
