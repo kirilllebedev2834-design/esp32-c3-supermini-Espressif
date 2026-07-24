f336a438b6f24540ad4c8bf67d3b07383ca2711f
Kirill76 <kirilllebedev2834@gmail.com>
2026-07-24 15:08:24 +0300

    написаны функции uart_print и warning_led

--- bootloader/src/macrosandother.h
+++ bootloader/src/macrosandother.h
@@ -14,24 +14,26 @@
 #define true ((_Bool)1)
 #define false ((_Bool)0)
 #define bool _Bool
+#define REGISTER_POINTER(adress) (*(volatile uint32_t*)(adress))
 
 // UART macroses for work from documentation:
 #define UART_FIFO_REG 0x60000000  // Буфер приема/передачи (FIFO)
 #define UART_CLKDIV_REG 0x60000014  // Делитель частоты (скорость)
-#define UART_CONF0_REG  0x60000020  // Конфигурация кадра
+#define UART_STATUS_REGISTER 0x6000001C
+#define UART_CONF_REG  0x60000020  // Конфигурация кадра
 #define UART_CLK_CONF_REG 0x60000078  // Настройка частоты ядра
 #define UART_UPDATE_REG 0x600000A4  // Синхронизация регистров
-#define SYS_CLK_EN0_REG 0x600C0010  // Включение тактирования периферии
-#define SYS_RST_EN0_REG 0x600C0018  // Сброс периферии
+#define SYS_CLK_EN_REG 0x600C0010  // Включение тактирования периферии
+#define SYS_RST_EN_REG 0x600C0018  // Сброс периферии
 
-//GPIO and IO_MUX macroses for work from documentation: 
+//GPIO and IO_MUX macroses for work from documentation:
+#define GPIO_OUTPUT_SET_REGISTER 0x60004008 
+#define GPIO_OUTPUT_CLEAR_REGISTER 0x6000400C
 #define GPIO_OUTPUT_ENABLE_REGISTER 0x60004020
 #define GPIO_OUTPUT_ENABLE_SET_REGISTER 0x60004024
 #define GPIO_OUTPUT_ENABLE_CLEAR_REGISTER 0x60004028
 #define IO_MUX_CONF_REG_FOR_PIN_GPIO9 0x60009028
 #define IO_MUX_CONF_REG_FOR_PIN_GPIO8 0x60009024
-#define IO_MUX_CONF_REG_FOR_PIN_GPIO5 0x6000901C
-#define IO_MUX_CONF_REG_FOR_PIN_GPIO4 0x60009018
 
 typedef uint8_t byte;
 
@@ -42,7 +44,7 @@ typedef enum
    SYSTEM_TIMEOUT = 1,
    SYSTEM_INVALID_ARG = 2,
    SYSTEM_PER_IS_NOT_READY = 3,
-   SYSTEM_INVALID_JUMP_TO_PROGRAM = 4
+   SYSTEM_INVALID_JUMP_TO_PROGRAM = 4,
 } error_status_t; 
 
 
@@ -51,8 +53,30 @@ extern void blink_function(uint16_t count, uint32_t delay_milliseconds);
 //extern void 
 void init_uart1(void);
 void init_gpio(void);
-void led_on(void);
+void warning_led(void);
 void uart_print(const char* string);
 void sys_print_error(const char* message);
 
+/*
+   warning_led - функция которая будет использоваться для мигания в ситуациях когда у нас возникают ошибки 
+   включает светодиод на 3 секунды и потом выключает его предупреждая что случилась ошибка.
+*/
+void warning_led(void)
+{
+   REGISTER_POINTER(GPIO_OUTPUT_SET_REGISTER) = (1 << 8);
+   delay(3000);
+   REGISTER_POINTER(GPIO_OUTPUT_CLEAR_REGISTER) = (1 << 8);
+}
+/*uart_print - функция которая печатает символы через UART0 в терминал для вывода ошибок*/
+void uart_print(const char* string) 
+{
+    while (*string) 
+    {
+        // Ждем, если буфер почти полон
+        while (((REGISTER_POINTER(UART_STATUS_REGISTER) >> 16) & 0xFF) >= 120) {}
+        
+        // Отправляем символ
+        REGISTER_POINTER(UART_FIFO_REG) = *string++;
+    }
+}
 #endif
