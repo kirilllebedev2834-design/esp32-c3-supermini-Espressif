5dffc261421a46cb84bac0b60756308e8a299aa5
Kirill76 <kirilllebedev2834@gmail.com>
2026-07-25 18:31:58 +0300

    изменен

--- program2/src/main.c
+++ program2/src/main.c
@@ -14,8 +14,26 @@
     г) Каждую секунду мигает светодиод показывая о работе во второй программе 
     д) Обработка ошибок: надёжная обработка возможных ошибок
 */
+#include "macrosandotherprogram2.h"
+
+void waiting_button_for_jump(void)
+{
+    static const byte gpio_num = GPIO_NUM_9;
+    bool is_button_press = (button_function(1 << gpio_num) != 0);
+    while(!is_button_press)
+    {
+        blink_function(BLINK_TIME);
+        for(byte i = 0; i < 6; i++)
+        {
+            if(button_function(1 << gpio_num) != 0) { return; }
+            delay(PAUSE);
+        }  
+    }
+}
 
 void app_main() 
 {
-    
+    for(byte i = 0; i < 2; i++){ blink_function(500); }
+    waiting_button_for_jump();
+
 }
